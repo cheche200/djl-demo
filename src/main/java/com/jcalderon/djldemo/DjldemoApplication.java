eb837f9722c84b422922558f94d1c5573e36739a
Jose Calderon <josecalderon@Joses-Air.lan>
2022-03-06 00:15:33 +0000

    Added simple demo

---
+++ src/main/java/com/jcalderon/djldemo/DjldemoApplication.java
@@ -0,0 +1,13 @@
+package com.jcalderon.djldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcalderon/djldemo/DjldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcalderon.djldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
